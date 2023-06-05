d101c8b3ce0ca780604f58f9e83954260549659b
IBRAHIM KUS <80965145+ibrahimkus89@users.noreply.github.com>
2023-06-05 12:59:57 +0300

    line script and line renderer

---
+++ Draw-A-Road/Assets/Script/DrawLine.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour
+{
+    [SerializeField] private LineRenderer _LineRenderer;
+    [SerializeField] private EdgeCollider2D _EdgeCollider;
+    [SerializeField]  List<Vector2> FingerPositionList=new();
+
+    private Camera _Camera;
+    private bool LineStart;
+
+    void Start()
+    {
+        _Camera =Camera.main;
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !LineStart)
+        {
+            CreateLine();
+            LineStart = true;
+        }
+
+        if (Input.GetMouseButton(0) && LineStart)
+        {
+            Vector2 FingerPosition = _Camera.ScreenToWorldPoint(Input.mousePosition);
+
+            if (Vector2.Distance(FingerPosition, FingerPositionList[^1]) >.1f)
+            {
+                UpdateLine(FingerPosition);
+
+            }
+        }
+    }
+
+    void CreateLine()
+    {
+        FingerPositionList.Clear();
+        FingerPositionList.Add(_Camera.ScreenToWorldPoint(Input.mousePosition));
+        FingerPositionList.Add(_Camera.ScreenToWorldPoint(Input.mousePosition));
+
+        _LineRenderer.SetPosition(0, FingerPositionList[0]);
+        _LineRenderer.SetPosition(1, FingerPositionList[1]);
+        _EdgeCollider.points = FingerPositionList.ToArray();
+
+    }
+
+
+    void UpdateLine(Vector2 incomingFingerPosition)
+    {
+        FingerPositionList.Add(incomingFingerPosition);
+        _LineRenderer.positionCount++;
+        _LineRenderer.SetPosition(_LineRenderer.positionCount-1,incomingFingerPosition);
+        _EdgeCollider.points = FingerPositionList.ToArray();
+
+    }
+}
