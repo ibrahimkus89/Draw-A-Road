0dce089671460174da5a0a1b67949272309d0b40
IBRAHIM KUS <80965145+ibrahimkus89@users.noreply.github.com>
2023-06-05 12:03:39 +0300

    project settings

---
+++ Draw-A-Road/Assets/Script/Kutuphane.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OlcayKalyoncuoglu
+{
+    /*
+    public class GenelYonetim
+    {
+        public static GameManager _GameManager;
+
+        public GenelYonetim(GameManager _gameManager) {
+
+            _GameManager = _gameManager;
+        }
+    }
+
+   
+    //-- BELLEK YÖNETÝMÝ
+    public static class BellekYonetimi
+    {
+
+        public static void VeriKaydetInt(string Key,int Value)
+        {
+            PlayerPrefs.SetInt(Key, Value);
+        }
+
+        public static int VeriOkuInt(string Key)
+        {
+            return PlayerPrefs.GetInt(Key);
+        }
+
+
+        public static void VeriKaydetString(string Key, string Value)
+        {
+            PlayerPrefs.SetString(Key, Value);
+        }
+
+        public static string VeriOkuString(string Key)
+        {
+            return PlayerPrefs.GetString(Key);
+        }
+
+        public static bool AnahtarVarmi(string Key)
+        {
+            return PlayerPrefs.HasKey(Key);
+
+        }
+
+
+    }
+
+    */
+}
